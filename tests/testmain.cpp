414caa88537cabd4dc1665e22705dd46795960a9
Tim Bishop <timbishop.csci@gmail.com>
2024-07-19 21:34:30 -0400

    blanking tests until there is actually something to test

--- tests/testmain.cpp
+++ tests/testmain.cpp
@@ -2,13 +2,15 @@
 #define BOOST_TEST_MAIN
 #include <boost/test/unit_test.hpp>
 
+#include <spdlog/spdlog.h>
+
 // LOGGER TESTS
 
 BOOST_AUTO_TEST_SUITE( logger_tests )
 
 BOOST_AUTO_TEST_CASE( logger_no_error )
 {
-    BOOST_WARN_MESSAGE( 0, "HI THERE" );
+    
 }
 
 BOOST_AUTO_TEST_SUITE_END()
--- tests/testother.cpp
+++ tests/testother.cpp
@@ -1,12 +1,14 @@
 #include <boost/test/unit_test.hpp>
 
+
+
 // LOGGER TESTS
 
 BOOST_AUTO_TEST_SUITE( other_tests )
 
 BOOST_AUTO_TEST_CASE( eeeee )
 {
-    BOOST_WARN_MESSAGE( 0, "HELLO THERE" );
+    
 }
 
 BOOST_AUTO_TEST_SUITE_END()
