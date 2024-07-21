94cbe11457fc7a33b94e0cc8d8452408b33def4f
Tim Bishop <timbishop.csci@gmail.com>
2024-07-21 11:25:44 -0400

    added a couple of tests for components and edited CONTRIBUTING.md

---
+++ tests/Components/TestRectangleColliderComponent.cpp
@@ -0,0 +1,22 @@
+#include <boost/test/unit_test.hpp>
+
+#include <ECS/Roc_ECS.hpp>
+
+BOOST_AUTO_TEST_SUITE( Component_Tests )
+
+BOOST_AUTO_TEST_CASE( RectangleColliderComponent_Tests )
+{
+    // Ensure offset properties exist
+    RectangleCollider rc;
+    BOOST_CHECK_NO_THROW( rc.offsetX = 0.5 );
+    BOOST_CHECK_NO_THROW( rc.offsetY = 0.5 );
+
+    // Ensure size properties exist
+    BOOST_CHECK_NO_THROW( rc.width = 5.0 );
+    BOOST_CHECK_NO_THROW( rc.height = 5.0 );
+
+    // Ensure collision vector exists
+    BOOST_CHECK_NO_THROW( rc.collisions.emplace_back(Collision()) );
+}
+
+BOOST_AUTO_TEST_SUITE_END()
