f0df71d2a2ced1ed7eb69b1989fa35002738049f
Tim Bishop <timbishop.csci@gmail.com>
2024-07-21 11:00:40 -0400

    finished ecs tests

--- tests/TestECS.cpp
+++ tests/TestECS.cpp
@@ -10,6 +10,7 @@ struct ECS_Fixture
     {
         Coordinator* c = Coordinator::Get();
         c->Init();
+        c->RegisterComponent<Transform>();
         c->RegisterComponent<Gravity>();
         c->CreateEntity("test_ent");
         c->CreateEntity("test_entity2");
@@ -81,6 +82,28 @@ BOOST_FIXTURE_TEST_CASE( test_component_commands, ECS_Fixture )
     BOOST_TEST( c->RemoveComponent<Gravity>(c->GetEntity("test_ent")) );
     BOOST_CHECK_THROW( c->GetComponent<Gravity>(c->GetEntity("test_ent")),
                        std::runtime_error );
+}
+
+BOOST_FIXTURE_TEST_CASE( test_system_commands, ECS_Fixture )
+{
+    // Sanity check, do all entities exist still?
+    Coordinator* c = Coordinator::Get();
+    BOOST_TEST( c->GetEntity("test_ent") == 0 );
+    BOOST_TEST( c->GetEntity("test_entity2") == 1 );
+
+    // Startup check - register RectangleCollider for CollisionSystem
+    BOOST_TEST( c->RegisterComponent<RectangleCollider>() );
+
+
+    // Does registering a System return a shared ptr to the system?
+    auto sysptr = c->RegisterSystem<CollisionSystem>();
+    BOOST_CHECK_NO_THROW( sysptr->GetSignature() );
+
+    // Does adding a necessary component to an entity add it to mEntities?
+    BOOST_CHECK( c->GetSystem<CollisionSystem>()->mEntities.size() == 0 );
+    BOOST_CHECK_NO_THROW( c->AddComponent<RectangleCollider>(c->GetEntity("test_ent"), RectangleCollider()) );
+    BOOST_CHECK_NO_THROW( c->AddComponent<Transform>(c->GetEntity("test_ent"), Transform()) );
+    BOOST_CHECK( c->GetSystem<CollisionSystem>()->mEntities.size() == 1 );
 }
 
 
